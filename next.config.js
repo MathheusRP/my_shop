3bdcdd64d78be2cb6f2a287f9183031a0cc4565e
Matheus <mathheus.rp111@gmail.com>
2023-11-14 13:33:49 -0300

    feat: config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "img.terabyteshop.com.br", 
+      "cdn.pichau.com.br", 
+      "dlcdnwebimgs.asus.com", 
+      "images.tcdn.com.br",
+      "shopinfo.vteximg.com.br",
+      "www.mercanteexpress.com.br",
+      "m.media-amazon.com",
+      "static.cdnlive.com.br",
+      "b2bleonorashop.vtexassets.com",
+    ],
+    // remotePatterns: [
+    //     {
+    //         protocol: 'https',
+    //         hostname: '**',
+    //         port: '',
+    //         pathname: '**',
+    //     },
+    // ],
+},
+
 }
 
+
+
 module.exports = nextConfig
+
